925480b7275976f8ddbf959cf24650d0ffb296b6
luododo <1619102340@qq.com>
2020-04-08 20:38:38 +0800

    springboot+cloud+vue 讲师模块提交01

---
+++ service/service_edu/src/main/java/com/ithr/eduservice/controller/EduLoginController.java
@@ -0,0 +1,24 @@
+package com.ithr.eduservice.controller;
+
+import com.ithr.commonutils.Result;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author com.hr.Text
+ * @date 2020/4/8 - 17:02
+ */
+@RestController
+@RequestMapping("/eduservice/user")
+@CrossOrigin//解决跨域
+public class EduLoginController {
+    //login
+    @PostMapping("login")
+    public Result login(){
+        return Result.ok().data("token","admin");
+    }
+    //info
+    @GetMapping("info")
+    public Result info(){
+        return Result.ok().data("roles","[admin]").data("name","admin").data("avatar","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+    }
+}
