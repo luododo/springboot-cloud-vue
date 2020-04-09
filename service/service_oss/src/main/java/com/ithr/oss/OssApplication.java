3dbf00af718b7d55460a6e941c5e5b181df3c552
luododo <1619102340@qq.com>
2020-04-09 19:40:54 +0800

    springboot+cloud+vue 测试

--- service/service_oss/src/main/java/com/ithr/oss/OssApplication.java
+++ service/service_oss/src/main/java/com/ithr/oss/OssApplication.java
@@ -2,12 +2,13 @@ package com.ithr.oss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author com.hr.Text
  * @date 2020/4/9 - 17:49
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class OssApplication {
     public static void main(String[] args) {
         SpringApplication.run(OssApplication.class,args);
